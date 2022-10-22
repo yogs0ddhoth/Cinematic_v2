30224fb726978e995ccffe5fffd2f1bc9e435ba0
yogs0ddhoth <blin7.webdev@gmail.com>
2022-10-22 01:38:51 -0400

    axios instance created in context

--- server/context.ts
+++ server/context.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import axios, { AxiosInstance } from 'axios';
+
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 export interface Context {
     prisma: PrismaClient;
+    axios: AxiosInstance
 }
 
 export const context: Context = {
-    prisma: new PrismaClient()
+    prisma: new PrismaClient(),
+    axios: axios.create({
+        baseURL: 'https://imdb-api.com/API/AdvancedSearch/' + process.env.IMDB_KEY1
+    })
 };
