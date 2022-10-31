bb7667c60339de55aae924ccdb5749332e7257bc
yogs0ddhoth <blin7.webdev@gmail.com>
2022-10-31 14:29:20 -0400

    routing scaffolding created

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApolloTestComponent } from './apollo-test/apollo-test.component';
+import { ImdbTestComponent } from './imdb-test/imdb-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ApolloTestComponent},
+  {path: 'test', component: ImdbTestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
