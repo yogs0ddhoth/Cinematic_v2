7df3ed9c1b74d12a8edd062693a3c585217edc22
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-06 12:51:33 -0500

    test font end

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cinematic';
+
+  modalActive: boolean = false;
+
+  bg: string = ''
+
+  toggle() {
+    this.modalActive = !this.modalActive;
+    this.bg = this.modalActive 
+    ? 'bg-red-600' 
+    : '';
+  }
+  // modal: string = this.modalActive
+  // ? 'ease-out duration-300 opacity-100 translate-y-0 sm:scale-100'
+  // : 'ease-in duration-200 opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+
 }
