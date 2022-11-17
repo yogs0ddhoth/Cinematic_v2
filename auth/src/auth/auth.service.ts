1ce519501f5459f9dc5ad0c1aa1f7c0c4060b060
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-17 17:06:41 -0500

    auth configured for passport

--- auth/src/auth/auth.service.ts
+++ auth/src/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
   #bcrypt = bcrypt;
+  constructor(private readonly userService: UserService) {}
 
   async validatePassword(password: string): Promise<string> {
     return this.#bcrypt.hash(password, 12);
+  }
+
+  async validateUser(email: string, password: string) {
+    const { password: hash, ...user } = await this.userService.user({ email });
+
+    if (await this.verifyUser(password, hash)) {
+      return user;
+    }
+
+    return null;
   }
 
   async verifyUser(password: string, userHash: string): Promise<boolean> {
