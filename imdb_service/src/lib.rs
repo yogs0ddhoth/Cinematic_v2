d51bfc7210c6e0372ae1ca32cfd61a191c8dd930
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-23 19:10:42 -0500

    Test passed

--- imdb_service/src/lib.rs
+++ imdb_service/src/lib.rs
@@ -1,21 +1,20 @@
-#[cfg(test)]
-mod tests {
-  use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
-  use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-  use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-  use dotenvy::dotenv;
-  use std::{env, collections::HashMap, error::Error};
-  use reqwest;
-  use serde::{Deserialize, Serialize};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use dotenvy::dotenv;
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, env, error::Error, process::Termination};
 
-  #[derive(Deserialize, Debug)]
-  pub struct AdvancedSearchData {
+#[derive(Deserialize, Debug)]
+pub struct AdvancedSearchData {
     pub queryString: Option<String>,
     pub results: Option<Vec<AdvancedSearchResult>>,
     pub errorMessage: Option<String>,
-  }
-  #[derive(Deserialize, Debug)]
-  pub struct AdvancedSearchResult {
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AdvancedSearchResult {
     pub id: Option<String>,
     pub image: Option<String>,
     pub title: Option<String>,
@@ -30,23 +29,31 @@ mod tests {
     pub plot: Option<String>,
     pub stars: Option<String>,
     pub starList: Option<Vec<HashMap<String, String>>>,
-  }
-  
-  #[tokio::main]
-  #[test]
-  async fn api_call_works() -> Result<(), Box<dyn Error>> {
-      dotenv().ok();
-      let imdb_key = env::var("IMDB_KEY")?;
-      println!("{:#?}", imdb_key);
+}
 
-      let url = format!("https://imdb-api.com/API/AdvancedSearch/{key}/?title={title}&sort=moviemeter,asc", key=imdb_key, title="inception");
-      println!("{:#?}", url);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-      let response = reqwest::get(url).await?;
-      println!("{:#?}", response);
+    #[tokio::main]
+    #[test]
+    async fn api_call_works() -> Result<(), Box<dyn Error>> {
+        dotenv().ok();
+        let imdb_key = env::var("IMDB_KEY")?;
+        println!("{:#?}", imdb_key);
 
-      let movies = response.json::<AdvancedSearchData>().await?;
-      println!("{:?}", movies);
-      Ok(())
-  }
+        let url = format!(
+            "https://imdb-api.com/API/AdvancedSearch/{key}/?title={title}&sort=moviemeter,asc",
+            key = imdb_key,
+            title = "inception"
+        );
+        println!("{:#?}", url);
+
+        let response = reqwest::get(url).await?;
+        println!("{:#?}", response);
+
+        let movies = response.json::<AdvancedSearchData>().await?;
+        println!("{:?}", movies);
+        Ok(())
+    }
 }
--- imdb_service/src/main.rs
+++ imdb_service/src/main.rs
@@ -1,9 +1,9 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use dotenvy::dotenv;
-use std::{env, collections::HashMap, error::Error};
 use reqwest;
+use std::{collections::HashMap, env, error::Error};
 struct Query;
 
 fn main() {}
