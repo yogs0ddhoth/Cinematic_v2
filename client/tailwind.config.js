845b8ddc739e4e970bf44fd9fca09c1a6feb5a9a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-29 20:54:13 -0500

    tailwind disabled to avoid bootstrap conflicts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  // corePlugins: {
-  //   preflight: false,
-  // },
-  // purge: {
-  //   enabled: true,
-  //   content: ['./src/**/*.{html,ts}']
-  // }
-}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,ts}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   // corePlugins: {
+//   //   preflight: false,
+//   // },
+//   // purge: {
+//   //   enabled: true,
+//   //   content: ['./src/**/*.{html,ts}']
+//   // }
+// }
