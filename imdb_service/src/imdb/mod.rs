bfb0f1e3501121e43936400ddad134a78b713008
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-30 18:11:29 -0500

    imdb call modularized

---
+++ imdb_service/src/imdb/mod.rs
@@ -0,0 +1,55 @@
+use std::{collections::HashMap, env};
+use dotenvy::dotenv;
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+pub struct AdvancedSearchData {
+    pub queryString: Option<String>,
+    pub results: Option<Vec<AdvancedSearchResult>>,
+    pub errorMessage: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AdvancedSearchResult {
+    pub id: Option<String>,
+    pub image: Option<String>,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub runtimeStr: Option<String>,
+    pub genres: Option<String>,
+    pub genreList: Option<Vec<HashMap<String, String>>>,
+    pub contentRating: Option<String>,
+    pub imDbRating: Option<String>,
+    pub imDbRatingVotes: Option<String>,
+    pub metacriticRating: Option<String>,
+    pub plot: Option<String>,
+    pub stars: Option<String>,
+    pub starList: Option<Vec<HashMap<String, String>>>,
+}
+
+#[tokio::main]
+pub async fn call_imdb(title: &str) -> Result<AdvancedSearchData, reqwest::Error> {
+    dotenv().ok();
+    let env_imdb_key = env::var("IMDB_KEY");
+    let mut imdb_key = match env_imdb_key {
+        Ok(data) => data,
+        Err(data) => {
+            println!("Error getting Imdb Key: {:#?}", data);
+            data.to_string()
+        }
+    };
+
+    let url = format!(
+        "https://imdb-api.com/API/AdvancedSearch/{key}/?title={title}&sort=moviemeter,asc",
+        key = imdb_key,
+        title = title
+    );
+    println!("{:#?}", url);
+
+    let response = reqwest::get(url).await?;
+
+    let movies = response.json::<AdvancedSearchData>().await?;
+
+    Ok(movies)
+}
--- imdb_service/src/lib.rs
+++ imdb_service/src/lib.rs
@@ -1,3 +1,9 @@
+mod imdb;
+
+use imdb::{
+    AdvancedSearchData, AdvancedSearchResult, call_imdb
+};
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
@@ -6,54 +12,17 @@ use reqwest;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, env, error::Error, process::Termination};
 
-#[derive(Deserialize, Debug)]
-pub struct AdvancedSearchData {
-    pub queryString: Option<String>,
-    pub results: Option<Vec<AdvancedSearchResult>>,
-    pub errorMessage: Option<String>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct AdvancedSearchResult {
-    pub id: Option<String>,
-    pub image: Option<String>,
-    pub title: Option<String>,
-    pub description: Option<String>,
-    pub runtimeStr: Option<String>,
-    pub genres: Option<String>,
-    pub genreList: Option<Vec<HashMap<String, String>>>,
-    pub contentRating: Option<String>,
-    pub imDbRating: Option<String>,
-    pub imDbRatingVotes: Option<String>,
-    pub metacriticRating: Option<String>,
-    pub plot: Option<String>,
-    pub stars: Option<String>,
-    pub starList: Option<Vec<HashMap<String, String>>>,
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[tokio::main]
     #[test]
-    async fn api_call_works() -> Result<(), Box<dyn Error>> {
-        dotenv().ok();
-        let imdb_key = env::var("IMDB_KEY")?;
-        println!("{:#?}", imdb_key);
+    fn call_api_works() {
+        let movies = call_imdb("inception");
 
-        let url = format!(
-            "https://imdb-api.com/API/AdvancedSearch/{key}/?title={title}&sort=moviemeter,asc",
-            key = imdb_key,
-            title = "inception"
-        );
-        println!("{:#?}", url);
-
-        let response = reqwest::get(url).await?;
-        println!("{:#?}", response);
-
-        let movies = response.json::<AdvancedSearchData>().await?;
-        println!("{:?}", movies);
-        Ok(())
+        match movies {
+            Ok(data) => println!("{:#?}", data),
+            Err(data) => println!("{:#?}", data),
+        }
     }
 }
