e816fac87073273a15c71aaedc790a5cce05b238
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-29 20:53:08 -0500

    components imported

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,18 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cinematic';
+  // title = 'cinematic';
 
-  modalActive: boolean = false;
+  // modalActive: boolean = false;
 
-  bg: string = ''
+  // bg: string = ''
 
-  toggle() {
-    this.modalActive = !this.modalActive;
-    this.bg = this.modalActive 
-    ? 'bg-red-600' 
-    : '';
-  }
+  // toggle() {
+  //   this.modalActive = !this.modalActive;
+  //   this.bg = this.modalActive 
+  //   ? 'bg-red-600' 
+  //   : '';
+  // }
   // modal: string = this.modalActive
   // ? 'ease-out duration-300 opacity-100 translate-y-0 sm:scale-100'
   // : 'ease-in duration-200 opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 import { ApolloTestComponent } from './apollo-test/apollo-test.component';
 import { ImdbTestComponent } from './imdb-test/imdb-test.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApolloTestComponent,
-    ImdbTestComponent
+    ImdbTestComponent,
+    LayoutComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule
