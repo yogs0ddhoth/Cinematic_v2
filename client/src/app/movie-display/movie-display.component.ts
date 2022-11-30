dfdc36f208ca36c24977da168ab2e4f030cabe92
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-29 21:33:09 -0500

    movie-display created

---
+++ client/src/app/movie-display/movie-display.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-movie-display',
+  templateUrl: './movie-display.component.html',
+  styleUrls: ['./movie-display.component.css'],
+})
+export class MovieDisplayComponent implements OnInit {
+  modalRef?: BsModalRef;
+  constructor(private readonly modalService: BsModalService) {}
+
+  ngOnInit(): void {}
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+}
