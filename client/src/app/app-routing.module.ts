9d5dae098ca247de096407720cafa4b023995f8a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-11-29 21:17:19 -0500

    movieDisplay added to router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApolloTestComponent } from './apollo-test/apollo-test.component';
 import { ImdbTestComponent } from './imdb-test/imdb-test.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
 
 const routes: Routes = [
-  {path: '', component: ApolloTestComponent},
-  {path: 'test', component: ImdbTestComponent}
+  { path: '', component: MovieDisplayComponent },
+  { path: 'apollo', component: ApolloTestComponent },
+  { path: 'test', component: ImdbTestComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
