ff44b67204b778fd2049bdd8e76ad74e16f3f77d
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-01 00:16:29 -0500

    graphql server created

--- imdb_service/src/main.rs
+++ imdb_service/src/main.rs
@@ -1,9 +1,31 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{guard, web, App, HttpResponse, HttpServer, Responder};
 use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 use dotenvy::dotenv;
 use reqwest;
 use std::{collections::HashMap, env, error::Error};
-struct Query;
 
-fn main() {}
+use imdb_service::Query;
+
+async fn index(
+    schema: web::Data<Schema<Query, EmptyMutation, EmptySubscription>>,
+    request: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(request.into_inner()).await.into()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+
+    HttpServer::new(move || {
+        App::new().app_data(web::Data::new(schema.clone())).service(
+            web::resource("/graphql/movies")
+                .guard(guard::Get())
+                .to(index),
+        )
+    })
+    .bind("127.0.0.1:4001")?
+    .run()
+    .await
+}
