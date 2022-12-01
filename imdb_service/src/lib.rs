eb02f792b6a549e881ce0f7809d6b5739a5dadaa
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-01 00:17:05 -0500

    impl search for Query

--- imdb_service/src/lib.rs
+++ imdb_service/src/lib.rs
@@ -1,16 +1,43 @@
 mod imdb;
+// mod schema;
 
-use imdb::{
-    AdvancedSearchData, AdvancedSearchResult, call_imdb
-};
+use imdb::{call_imdb, AdvancedSearchData, Movie};
+// use schema::{};
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use dotenvy::dotenv;
-use reqwest;
-use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, env, error::Error, process::Termination};
+
+use std::{
+    clone,
+    // collections::HashMap,
+    // env,
+    error::Error,
+    process::Termination,
+};
+
+pub struct Query;
+
+#[Object]
+impl Query {
+    async fn search(&self) -> Vec<Movie> {
+        let mut results: Vec<Movie> = Vec::new();
+        let response = call_imdb("inception");
+        match response {
+            Ok(data) => match data.results {
+                Some(mut data) => {
+                    results.append(&mut data);
+                    results
+                }
+                None => results,
+            },
+            Err(data) => {
+                println!("Error getting Imdb data: {:#?}", data);
+                results
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
