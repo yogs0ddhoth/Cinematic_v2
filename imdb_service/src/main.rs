0aa2cfccc264a3b776efaa820d177a63eaeee84e
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-02 00:36:21 -0500

    playground added, server functional

--- imdb_service/src/main.rs
+++ imdb_service/src/main.rs
@@ -1,9 +1,6 @@
-use actix_web::{guard, web, App, HttpResponse, HttpServer, Responder};
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, ID};
+use actix_web::{guard, web, App, HttpResponse, HttpServer};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, http::{playground_source, GraphQLPlaygroundConfig}};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use dotenvy::dotenv;
-use reqwest;
-use std::{collections::HashMap, env, error::Error};
 
 use imdb_service::Query;
 
@@ -14,16 +11,29 @@ async fn index(
     schema.execute(request.into_inner()).await.into()
 }
 
+async fn graphql_playground() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
 
+    println!("Listening at: http://localhost:4001");
+
     HttpServer::new(move || {
-        App::new().app_data(web::Data::new(schema.clone())).service(
-            web::resource("/graphql/movies")
+        App::new().app_data(web::Data::new(schema.clone()))
+            .service(
+                web::resource("/")
                 .guard(guard::Get())
-                .to(index),
-        )
+                .to(graphql_playground))
+            .service(
+                web::resource("/")
+                    .guard(guard::Post())
+                    .to(index),
+            )
     })
     .bind("127.0.0.1:4001")?
     .run()
