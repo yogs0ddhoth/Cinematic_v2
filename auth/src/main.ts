5313d0453412ee83112754b16268350fe33e89db
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-05 15:01:16 -0500

    logging added

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = 4001;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4001);
+
+  await app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
 }
 bootstrap();
