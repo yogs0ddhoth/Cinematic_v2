5b72dcefccd9bd34b69f178c70eaea4dcab9d1d3
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-06 21:45:29 -0500

    port now 4002

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = 3001;
+  const PORT = 4002;
   const app = await NestFactory.create(AppModule);
 
   await app.listen(PORT, () => {
