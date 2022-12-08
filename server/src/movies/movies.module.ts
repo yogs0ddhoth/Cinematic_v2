220eaca2019a414a83fed9708404512b3c12bb34
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-07 23:55:45 -0500

    path update to reflect prisma dir moving

--- server/src/movies/movies.module.ts
+++ server/src/movies/movies.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesResolver } from './movies.resolver';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import StarsService from './stars.service';
 import GenresService from './genres.service';
 import UsersService from './users.service';
