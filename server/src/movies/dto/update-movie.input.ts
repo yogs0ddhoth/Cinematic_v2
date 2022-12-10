c85bd1ec81552b825a312343ca12aa1555b98018
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-10 15:35:59 -0500

    migration complete

--- server/src/movies/dto/update-movie.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateMovieInput } from './create-movie.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateMovieInput extends PartialType(CreateMovieInput) {
-  id: number;
-}
