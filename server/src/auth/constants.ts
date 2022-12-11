25f70c31ea44380b3a894b5f37a97b385dc2d33c
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-11 12:21:19 -0500

    constants added

---
+++ server/src/auth/constants.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.SECRET_KEY) throw new Error('Secret Key Undefined');
+if (!process.env.NAMESPACE) throw new Error('Namespace Undefined');
+
+export const jwtConstants = {
+  secret: process.env.SECRET_KEY,
+};
+export const namespace = process.env.NAMESPACE;
