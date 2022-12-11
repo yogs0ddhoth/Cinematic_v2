0475c248160b6f37fdab87abc3524053a6fa7ac8
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-11 12:23:28 -0500

    auth service created

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/models/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+}
