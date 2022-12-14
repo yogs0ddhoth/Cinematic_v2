b3ec9beaeda9979637df8fa56e5049c0aa6c98d5
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-13 23:48:26 -0500

    dto update

--- server/src/models/movie/dto/create-movie.dto.ts
+++ server/src/models/movie/dto/create-movie.dto.ts
@@ -4,11 +4,11 @@ export class CreateMovie {
   readonly title: string;
   readonly description?: string | null;
   readonly runtimeStr?: string | null;
-  readonly genres?: string | null;
+  readonly genres?: string[] | null;
   readonly contentRating?: string | null;
   readonly imDbRating?: string | null;
   readonly imDbRatingVotes?: string | null;
   readonly metacriticRating?: string | null;
   readonly plot?: string | null;
-  readonly stars?: string | null;
+  readonly stars?: string[] | null;
 }
