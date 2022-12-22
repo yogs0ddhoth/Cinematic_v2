9a317a69e84416c8249fef4d063a112ad406b52a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-22 14:16:26 -0500

    concurrent reqs moved to trait, error handling added

--- movie_search_service/src/lib.rs
+++ movie_search_service/src/lib.rs
@@ -1,21 +1,15 @@
 pub mod imdb;
 pub mod omdb;
-
 pub mod schema;
 
-use actix_web::{error::HttpError, http::Error};
 use async_graphql::Object;
 use async_trait::async_trait;
-use futures::{
-    future,
-    stream::{self, StreamExt},
-};
+use futures::future;
 use omdb::{OMDBSearchData, OMDBSearchResult, OMDbMovie};
 use reqwest;
 use schema::{Movie, SearchMovieInput};
 use serde;
-use std::{borrow::Borrow, env, result};
-use tokio;
+use std::env;
 
 #[async_trait]
 trait Request {
@@ -23,29 +17,36 @@ trait Request {
         &self,
         url: &String,
     ) -> Result<T, reqwest::Error>;
+
+    async fn send_many_get_requests<T: for<'de> serde::Deserialize<'de> + std::marker::Send>(
+        &self,
+        urls: Vec<String>,
+    ) -> Vec<Result<T, reqwest::Error>>;
 }
 
 #[async_trait]
 impl Request for reqwest::Client {
-    /// build and send GET request to url, deserialize response to type parameter, and return results
+    /// Build and send GET request to url, deserialize response to type parameter, propogate error, and return results
     async fn send_get_request<T: for<'de> serde::Deserialize<'de>>(
         &self,
         url: &String,
     ) -> Result<T, reqwest::Error> {
-        println!("{:#?} Fetching...", url);
-        let response = match self.get(url).send().await {
-            Ok(data) => data,
-            Err(e) => return Err(e.without_url()),
-        };
-
-        println!("Fetched! Serializing response...");
-        let results = match response.json::<T>().await {
-            Ok(data) => data,
-            Err(e) => return Err(e.without_url()),
-        };
-
-        println!("Response Serialized. Sending data...");
+        println!("Fetching {:#?}...", url);
+        let results = self.get(url).send().await?.json::<T>().await?;
         Ok(results)
+    }
+    /// Concurrently send GET requests that deserialize to the same type, and return the results
+    async fn send_many_get_requests<T: for<'de> serde::Deserialize<'de> + std::marker::Send>(
+        &self,
+        urls: Vec<String>,
+    ) -> Vec<Result<T, reqwest::Error>> {
+        future::join_all(urls.iter().map(|url| async move {
+            match self.send_get_request::<T>(url).await {
+                Ok(result) => Ok(result),
+                Err(err) => Err(err),
+            }
+        }))
+        .await
     }
 }
 
@@ -57,53 +58,63 @@ impl Query {
         &self,
         search_movie_input: SearchMovieInput,
     ) -> Result<Vec<Movie>, reqwest::Error> {
-        // let mut imdb_results = imdb::call_imdb(fmt_imdb_url(search_movie_input)).await;
-
         let client = reqwest::Client::new();
 
-        // Create a Vec<String> of urls based on search_movie_input
-        let mut search_urls = Vec::new();
-        for i in 1..search_movie_input.pages {
-            // for pagination
-            search_urls.push(format!(
-                "{url}&page={num}",
-                url = search_movie_input.fmt_omdb_url(),
-                num = i
-            ));
-        }
-        let search_results = future::join_all(search_urls.iter().map(|url| {
-            // Concurrently make the requests, and extract releavant data. TODO: Handle possible Error Responses
-            let client = &client;
-            async move {
-                match client.send_get_request::<OMDBSearchData>(url).await {
-                    Ok(result) => match result.search {
-                        Some(data) => data,
-                        None => panic!(),
-                    },
-                    Err(_) => panic!(), // TODO: Add proper error handling
-                }
-            }
-        }))
-        .await;
-
-        // convert search_results into a Vec<String> of urls to fetch details of
         let mut id_urls = Vec::new();
-        search_results.iter().for_each(|results| {
-            for result in results {
-                id_urls.push(result.fmt_omdb_url())
+        {
+            // Search OMDB for all movies that match search_movie_input, and push the information to id_urls
+            let mut search_urls = Vec::new();
+            for i in 1..search_movie_input.pages {
+                // for pagination
+                // push urls for each page (see omdb api) to search_urls
+                search_urls.push(format!(
+                    "{url}&page={num}",
+                    url = search_movie_input.fmt_omdb_url(),
+                    num = i
+                ));
             }
-        });
-        let movie_results = future::join_all(id_urls.iter().map(|url| {
-            // Concurrently make the requests, and extract releavant data. TODO: Handle possible Error Responses
-            let client = &client;
-            async move {
-                match client.send_get_request::<OMDbMovie>(url).await {
-                    Ok(movie) => movie,
-                    Err(_) => panic!(),
-                }
-            }
-        }))
-        .await;
+
+            // concurrently send requests for each of the urls
+            client
+                .send_many_get_requests::<OMDBSearchData>(search_urls)
+                .await
+                .iter()
+                .for_each(|results| {
+                    // Filter out error responses and convert search_results into imdb ids of type String and push to id_urls
+                    println!("Filtering results...");
+                    match results {
+                        Ok(data) => match &data.search {
+                            Some(result_page) => {
+                                for result in result_page {
+                                    id_urls.push(result.fmt_omdb_url())
+                                }
+                            }
+                            None => match &data.error {
+                                Some(err) => println!("Found an Error: {}", err),
+                                None => (),
+                            },
+                        },
+                        Err(err) => println!("Found an Error: {:#?}", err),
+                    }
+                });
+        }
+
+        let mut movies = Vec::new();
+        {
+            // concurrently send requests for each of the id url
+            client
+                .send_many_get_requests::<OMDbMovie>(id_urls)
+                .await
+                .iter()
+                .for_each(|result| {
+                    // Filter out error responses and push OMDbMovie to movies
+                    println!("Filtering results...");
+                    match result {
+                        Ok(movie) => movies.push(movie),
+                        Err(err) => println!("Found an Error: {:#?}", err),
+                    }
+                });
+        }
 
         /* *
          * TODO:
@@ -128,20 +139,22 @@ impl Query {
 
 pub trait FormatUrl {
     fn fmt_omdb_url(&self) -> String;
-}
-
-impl FormatUrl for SearchMovieInput {
-    fn fmt_omdb_url(&self) -> String {
-        let omdb_key = match env::var("OMDB_KEY") {
+    fn get_omdb_key(&self) -> String {
+        match env::var("OMDB_KEY") {
             Ok(data) => data,
             Err(data) => {
                 println!("Error getting OMDb Key: {:#?}", data);
                 String::from("NO_KEY")
             }
-        };
+        }
+    }
+}
+
+impl FormatUrl for SearchMovieInput {
+    fn fmt_omdb_url(&self) -> String {
         format!(
             "https://www.omdbapi.com/?apikey={key}&s={title}&y={year}",
-            key = omdb_key,
+            key = self.get_omdb_key(),
             title = self.title,
             year = self.release_year,
         )
@@ -149,16 +162,9 @@ impl FormatUrl for SearchMovieInput {
 }
 impl FormatUrl for OMDBSearchResult {
     fn fmt_omdb_url(&self) -> String {
-        let omdb_key = match env::var("OMDB_KEY") {
-            Ok(data) => data,
-            Err(data) => {
-                println!("Error getting OMDb Key: {:#?}", data);
-                String::from("NO_KEY")
-            }
-        };
         format!(
             "https://www.omdbapi.com/?apikey={key}&i={id}",
-            key = omdb_key,
+            key = self.get_omdb_key(),
             id = self.imdb_id,
         )
     }
@@ -168,6 +174,7 @@ impl FormatUrl for OMDBSearchResult {
 mod tests {
     use super::*;
     use dotenvy::dotenv;
+    use tokio;
 
     /// test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 2 filtered out; finished in 0.32s
     #[tokio::test]
@@ -180,15 +187,18 @@ mod tests {
             user_rating: String::default(),
             pages: 1,
         };
+        let test_search_movie_url = test_search_movie_input.fmt_omdb_url();
+
         let test_omdb_search_result = OMDBSearchResult {
             imdb_id: String::from("tt0076759"),
         };
+        let test_omdb_search_url = test_omdb_search_result.fmt_omdb_url();
 
         let client = reqwest::Client::new();
 
         let (omdb_results_1, omdb_results_2) = tokio::join!(
-            client.send_get_request::<OMDBSearchData>(test_search_movie_input.fmt_omdb_url()),
-            client.send_get_request::<OMDbMovie>(test_omdb_search_result.fmt_omdb_url()),
+            client.send_get_request::<OMDBSearchData>(&test_search_movie_url),
+            client.send_get_request::<OMDbMovie>(&test_omdb_search_url),
         );
         println!("{:#?}", omdb_results_1);
         println!("{:#?}", omdb_results_2);
