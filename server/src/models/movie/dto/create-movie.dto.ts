2efa1448709130949d2aa263e3ce7a6cf1a4044b
yogs0ddhoth <blin7.webdev@gmail.com>
2022-12-25 00:42:37 -0500

    CreateType updated according to schema

--- server/src/models/movie/dto/create-movie.dto.ts
+++ server/src/models/movie/dto/create-movie.dto.ts
@@ -1,17 +1,25 @@
-import { Types } from 'mongoose';
-
 export class CreateMovie {
   readonly imDbID?: string | null;
-  readonly image?: string | null;
+
   readonly title: string;
-  readonly description?: string | null;
-  readonly runtimeStr?: string | null;
+  readonly year?: string | null;
+  readonly released?: string | null;
   readonly contentRating?: string | null;
-  readonly imDbRating?: string | null;
-  readonly imDbRatingVotes?: string | null;
-  readonly metacriticRating?: string | null;
+  readonly runtime?: string | null;
+
+  readonly director?: string | null;
+  readonly writer?: string | null;
+
   readonly plot?: string | null;
 
-  readonly genres?: Types.ObjectId[] | null;
-  readonly stars?: Types.ObjectId[] | null;
+  readonly language?: string | null;
+  readonly country?: string | null;
+  readonly awards?: string | null;
+  readonly image?: string | null;
+
+  readonly ratings?: { source: string; score: string }[] | null;
+  readonly imdbVotes?: string | null;
+
+  readonly boxOffice?: string | null;
+  readonly production?: string | null;
 }
