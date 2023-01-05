5b7e6f028dfb0928e05c65bc8653c2814eb0a4ed
yogs0ddhoth <blin7.webdev@gmail.com>
2023-01-05 00:52:52 -0500

    services and documents configured

--- server/src/models/models.module.ts
+++ server/src/models/models.module.ts
@@ -4,11 +4,15 @@ import { Genre, GenreSchema } from './schemas/genre.schema';
 import { Movie, MovieSchema } from './schemas/movie.schema';
 import { Actor, ActorSchema } from './schemas/actor.schema';
 import { User, UserSchema } from './schemas/user.schema';
+import { Director, DirectorSchema } from './schemas/director.schema';
+import { Writer, WriterSchema } from './schemas/writer.schema';
 
 import { GenreService } from './genre/genre.service';
 import { MovieService } from './movie/movie.service';
 import { ActorService } from './actor/actor.service';
 import { UserService } from './user/user.service';
+import { WriterService } from './writer/writer.service';
+import { DirectorService } from './director/director.service';
 
 @Module({
   imports: [
@@ -17,9 +21,25 @@ import { UserService } from './user/user.service';
       { name: Movie.name, schema: MovieSchema },
       { name: Genre.name, schema: GenreSchema },
       { name: Actor.name, schema: ActorSchema },
+      { name: Director.name, schema: DirectorSchema },
+      { name: Writer.name, schema: WriterSchema },
     ]),
   ],
-  providers: [GenreService, MovieService, ActorService, UserService],
-  exports: [GenreService, MovieService, ActorService, UserService],
+  providers: [
+    GenreService,
+    MovieService,
+    ActorService,
+    UserService,
+    WriterService,
+    DirectorService,
+  ],
+  exports: [
+    GenreService,
+    MovieService,
+    ActorService,
+    UserService,
+    WriterService,
+    DirectorService,
+  ],
 })
 export class ModelsModule {}
