684e943df58970c91d1aec1d952d5074539ba354
yogs0ddhoth <blin7.webdev@gmail.com>
2023-01-06 21:01:06 -0500

    documentation added

--- server/src/models/writer/writer.service.ts
+++ server/src/models/writer/writer.service.ts
@@ -11,6 +11,9 @@ import {
 import { Writer, WriterDocument } from '../schemas/writer.schema';
 import { CreateWriter } from './dto/create-writer.dto';
 
+/**
+ * Class containing Mongoose CRUD methods for interacting with the Writer collection
+ */
 @Injectable()
 export class WriterService {
   constructor(
@@ -19,19 +22,21 @@ export class WriterService {
   ) {}
 
   /**
-   * create a new Writer document or array of documents
+   * Mongoose.Model.create(): create a new Writer document or array of documents
    * @param {CreateWriter} doc document/documents to be created
    * @type {CreateWriter}  name: string, movieID?: Types.ObjectId | null }
-   * @returns created document or documents
+   * @returns created document or array documents
    */
   async create(doc: CreateWriter | CreateWriter[]) {
     return this.WriterModel.create(doc);
   }
 
   /**
-   * TODO: add documentation
-   * @param params
-   * @returns
+   * Mongoose Model.findOne(): gets a single document that matches filter
+   * @param filter fields to search by
+   * @param projection fields to return
+   * @param options query options - see docs
+   * @returns a single document, or null if none is found
    */
   async find(params: {
     filter: FilterQuery<WriterDocument>;
@@ -43,9 +48,11 @@ export class WriterService {
   }
 
   /**
-   * TODO: add documentation
-   * @param params
-   * @returns
+   * Mongoose Model.find(): gets a list of documents that match filter
+   * @param filter fields to search by
+   * @param projection fields to return
+   * @param options query options - see docs
+   * @returns a single document, array of documents, or null if none is found
    */
   async get(params: {
     filter: FilterQuery<WriterDocument>;
@@ -73,9 +80,9 @@ export class WriterService {
   }
 
   /**
-   * TODO: add documentation
-   * @param params
-   * @returns
+   * Mongoose Model.findByIdAndDelete(): atomically find the document by id and delete it
+   * @param params { id: string }
+   * @returns a document containing a boolean acknowledged as true if the operation ran with write concern or false if write concern was disabled, and deletedCount containing the number of deleted documents; or null if none is found
    */
   async delete(params: { id: string }) {
     const { id } = params;
