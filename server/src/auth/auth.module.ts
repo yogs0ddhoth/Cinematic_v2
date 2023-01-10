68209b654478049b1afee8900285c626e433d217
yogs0ddhoth <blin7.webdev@gmail.com>
2023-01-10 00:27:52 -0500

    unused service removed

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { ModelsModule } from 'src/models/models.module';
-import { AuthService } from './auth.service';
 import { jwtConstants } from '../constants';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    ModelsModule,
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60m' },
     }),
   ],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
