555d56b43e6e784840551d48b17f9c08565724ba
yogs0ddhoth <blin7.webdev@gmail.com>
2023-01-09 19:10:45 -0500

    methods and exception handling moved to AppService

--- auth/src/app.resolver.ts
+++ auth/src/app.resolver.ts
@@ -1,15 +1,11 @@
 // import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver, ResolveReference } from '@nestjs/graphql';
 // import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { AuthService } from 'src/auth/auth.service';
-import { UserService } from './user/user.service';
+import { AppService } from './app.service';
 
 @Resolver('User')
 export class AppResolver {
-  constructor(
-    private readonly userService: UserService,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   /** Login endpoint */
   @Mutation()
@@ -18,19 +14,9 @@ export class AppResolver {
     @Args('password') password: string,
   ) {
     try {
-      const user = await this.authService.validateUser(email, password);
-
-      if (!user)
-        throw new Error('Invalid Login Credentials', {
-          cause: {
-            value: { email, password },
-          },
-        });
-
-      return this.authService.login(user);
+      return await this.appService.login(email, password);
     } catch (error) {
       console.log(error);
-      return error;
     }
   }
 
@@ -41,25 +27,18 @@ export class AppResolver {
     @Args('password') password: string,
   ) {
     try {
-      const hash = await this.authService.validatePassword(password);
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { password: _, ...user } = await this.userService.createUser({
-        id: this.authService.generateUserId(email),
-        email,
-        password: hash,
-      });
-
-      return this.authService.login(user);
+      return await this.appService.signup(email, password);
     } catch (error) {
       console.log(error);
-      return error;
     }
   }
 
-  // TODO: protect route with passport jwt
   @ResolveReference()
-  resolveReference({ email }: { __typename: string; email: string }) {
-    return this.userService.user({ email });
+  async resolveReference({ id }: { __typename: string; id: string }) {
+    try {
+      return await this.appService.resolveUser(id);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
--- auth/src/app.service.ts
+++ auth/src/app.service.ts
@@ -1,8 +1,68 @@
 import { Injectable } from '@nestjs/common';
 
+import { AuthService } from './auth/auth.service';
+import { Auth, User } from './graphql';
+import { UserService } from './user/user.service';
+
+/**
+ * Class containing services for Auth
+ */
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
+
+  async login(email: string, password: string): Promise<Auth> {
+    try {
+      const user = await this.authService.validateUser(email, password);
+
+      if (!user)
+        throw new Error('Invalid Login Credentials', {
+          cause: {
+            value: { email, password },
+          },
+        });
+
+      return this.authService.login(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signup(email: string, password: string): Promise<Auth> {
+    try {
+      await this.authService.validateEmail(email);
+      const hash = await this.authService.validatePassword(password);
+
+      // remove password from user object
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...user } = await this.userService.createUser({
+        id: this.authService.generateUserId(email),
+        email,
+        password: hash,
+      });
+
+      return this.authService.login(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async resolveUser(id: string): Promise<User> {
+    try {
+      const user = await this.userService.user({ id });
+
+      if (!user)
+        throw new Error('User not found', {
+          cause: {
+            value: { id },
+          },
+        });
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 }
