dd7ac6ffef9802964af147d04384e9f40c15642f
Ben Lin <blin7.webdev@gmail.com>
2023-01-20 00:44:07 -0500

    display converted to dialog modal

--- client/src/app/components/movie-display/movie-display.component.ts
+++ client/src/app/components/movie-display/movie-display.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { Actor, Genre, Movie, Rating } from 'src/app/core/graph/generated';
+import { Component, Inject, OnInit, TemplateRef } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { Movie } from 'src/app/core/graph/generated';
 
 @Component({
   selector: 'app-movie-display',
@@ -8,29 +9,8 @@ import { Actor, Genre, Movie, Rating } from 'src/app/core/graph/generated';
   styleUrls: ['./movie-display.component.css'],
 })
 export default class MovieDisplayComponent implements OnInit {
-  modalRef?: BsModalRef;
-  @Input() movie!: Movie;
-  genres?: Genre[];
-  actors?: Actor[];
-  ratings?: Rating[];
-  image?: string;
-  trailers?: string[];
 
-  constructor(private readonly modalService: BsModalService) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Movie) {}
 
-  ngOnInit(): void {
-    const { genres, actors, ratings, trailers, image, ...movie } = this.movie;
-
-    if (genres) this.genres = genres;
-    if (actors) this.actors = actors;
-    if (ratings) this.ratings = ratings;
-    if (image) this.image = image;
-    if (trailers?.trailers?.length) this.trailers = trailers.trailers;
-
-    this.movie = movie;
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
-  }
+  ngOnInit(): void { }
 }
